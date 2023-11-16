6eb8cd6b8499ba3f17a24b2bd1fa21d751d7dd5a
RNOre <roman-adamovich@mail.ru>
2023-11-16 20:47:14 +0500

    change directory

--- backend/app/Models/IndivAchivBall.php
+++ backend/app/Models/IndivAchivBall.php
@@ -2,23 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class IndivAchivBall extends Model
+class IndivAchivBall
 {
-    use HasFactory;
 
-    public function typeIndivAchiv()
-    {
-        return $this->belongsTo(TypeIndivAchiv::class);
-    }
-    public function averageBall()
-    {
-        return $this->belongsTo(AverageBall::class);
-    }
-    public function student()
-    {
-        return $this->belongsTo(Student::class);
-    }
 }
